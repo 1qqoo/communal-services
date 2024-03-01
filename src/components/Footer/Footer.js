59851ac0e82702b77fe88f969c8ae8c5485b5127
unknown <musienko.roma@mail.ru>
2024-03-01 14:24:08 +0300

    foote

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Footer.css';
+
+import vk from '../../images/vk.svg';
+import tg from '../../images/tg.svg';
+import inst from '../../images/insta.svg';
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+        <div className='footer__container'>
+            <div className='footer__copyright'>
+                <p className='footer__text'>&#169; 
+                    <a className='footer__link' href='#'> https://your-site.ru </a> Все права защищены.
+                </p>
+                <p className='footer__text'>Копирование материалов только по письменному разрешению.</p>
+            </div>
+            <div className='footer__navigation'>
+                <p className='footer__text'>Мы онлайн:</p>
+                <div className='footer__box'>
+                    <img className='footer__icon' src={vk} alt='Вк' />
+                    <img className='footer__icon' src={tg} alt='Телеграм' />
+                    <img className='footer__icon' src={inst} alt='Инстаграм' />
+                </div>
+            </div>
+        </div>
+    </footer>
+  )
+}
